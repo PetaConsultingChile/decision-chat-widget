6abe6c75d39e3aafe352dba20c81209edd5c085b
Bitter <yunsde18@gmail.com>
2025-08-04 09:34:36 -0500

    chore: new info

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/main.tsx',
+      name: 'DecisionChatWidget',
+      fileName: () => `widget.js`,
+      formats: ['iife'] 
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+
 })
